7098c1df1e67efa3d601ab8e769c44c23b8fc8e6
Mircea Ioan Soit <mircea.soit@shopkick.com>
2021-01-21 09:15:04 +0200

    Some cleaning for MeteorsApiService.kt

--- app/src/main/java/workshop/mirceasoit/fallenmeteors/api/MeteorsApiService.kt
+++ app/src/main/java/workshop/mirceasoit/fallenmeteors/api/MeteorsApiService.kt
@@ -1,13 +1,10 @@
 package workshop.mirceasoit.fallenmeteors.api
 
 import io.reactivex.Observable
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import workshop.mirceasoit.fallenmeteors.model.Meteor
 
@@ -15,13 +12,8 @@ private const val BASE_URL = "https://data.nasa.gov/"
 
 private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
-    .addConverterFactory(ScalarsConverterFactory.create())
     .addConverterFactory(MoshiConverterFactory.create())
     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-    .client(
-        OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-        .build())
     .build()
 
 interface MeteorsApiService {
